5ebcb1c8c960906232cad31dd5b2b8f60ce70875
kawayi <kawayi_rendroid@163.com>
2018-11-19 14:27:21 +0000

    add test tfrecord

--- featureTests/LesssonTfrecord/test_tfrecord.py
+++ featureTests/LesssonTfrecord/test_tfrecord.py
@@ -3,7 +3,7 @@ import numpy as np
 #import skimage.io as io
 from skimage import io
 import cv2
-img=io.imread('D:\\work\\stuff\\modules\\misc\\sprd_camera\\alg\\july\\tf_base\\featureTests\\LesssonTfrecord\\miniVOC\\JPEGImages\\2007_000175.jpg')
+#filepath="D:\\work\\stuff\\modules\\misc\\sprd_camera\\alg\\july\\tf_base\\featureTests\\LesssonTfrecord\\miniVOC\\JPEGImages\\2007_000175.jpg"
+filepath="./miniVOC/JPEGImages/2007_000175.jpg"
+img=io.imread(filepath)
 io.imshow(img)
-cv2.waitKey (0)
-cv2.destroyAllWindows()
