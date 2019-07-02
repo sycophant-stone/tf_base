7617e7c23fca2c4ed4dd1d858960447ccb94ab50
hnren@aibee.com <kawayi_rendroid@163.com>
2019-07-02 06:51:22 +0000

    1. add bbox bounding 's vectorize methods 2. add np.where to figure out the correct value

--- refineDet/test_vectorize_metrics.py
+++ refineDet/test_vectorize_metrics.py
@@ -16,8 +16,9 @@ def calc_iou(prediction_bbox, gt_bbox):
            groudtruth boundingbox, [60, 4]
     """
     iou = 0
-    np.maxmium(prediction_bbox[:,0],gt_bbox[:,0])
-        xmax = gt_bbox[:,0]
+    #np.maxmium(prediction_bbox[:,0],gt_bbox[:,0])
+    if prediction_bbox[0]>gt_bbox[0]:
+        xmax = gt_bbox[0]
     else:
         xmax = prediction_bbox[0]
     if prediction_bbox[2]>gt_bbox[2]:
@@ -51,8 +52,34 @@ def calc_iou(prediction_bbox, gt_bbox):
     return iou    
     
 def find_col_maxvalue(prediction_bbox,gt_bbox):
-    newvect = np.maxmium(prediction_bbox[:,0],gt_bbox[:,0])   
-    print("find_col_maxvalue", newvect)
+    xmax_ = np.maximum(prediction_bbox[:,0],gt_bbox[:,0])
+    print("xmax_", xmax_)
+    print("xmax_'s shape", xmax_.shape)
+
+    ymax_ = np.maximum(prediction_bbox[:,1],gt_bbox[:,1])
+    print("ymax_", ymax_)
+    print("ymax_'s shape", ymax_.shape)
+
+    xmin_ = np.minimum(prediction_bbox[:,2],gt_bbox[:,2])
+    print("xmin_", xmin_)
+    print("xmin_'s shape", xmin_.shape)
+
+    ymin_ = np.minimum(prediction_bbox[:,3],gt_bbox[:,3])
+    print("ymin_", ymin_)
+    print("ymin_'s shape", ymin_.shape)
+
+    xcond_ = np.less(xmax_, xmin_)
+    print("xcond_", xcond_)
+    xleft_ = np.where(xcond_, xmax_, np.zeros(xcond_.shape))
+    print("xleft_", xleft_)
+
+    ycond_ = np.less(ymax_, ymin_)
+    print("ycond_", ycond_)
+    yleft_ = np.where(ycond_, ymax_, np.zeros(ycond_.shape))
+    print("yleft_", yleft_)
+
+
+
     return 
     
 def test_vectorize_calc_iou():
@@ -63,7 +90,8 @@ def test_vectorize_calc_iou():
        [300.77795 , -10.516249, 313.15857 ,  58.1938  ],
        [100.60458 , 146.97646 , 173.40833 , 161.21843 ],
        [86.82617 , 179.19148 , 134.84885 , 193.90288 ]], dtype="float32")
-    gt = np.array([[262.94,239,62.04001,70.40001]], dtype="float32")
+    #gt = np.array([[262.94,239,62.04001,70.40001]], dtype="float32")
+    gt = np.array([[62.04001,70.40001,262.94,239]], dtype="float32")
     print("pred shape:", pred.shape)
     print("pred value:", pred)
     print("gt shape:", gt.shape)
