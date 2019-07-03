e2d66ba9f8e98623dced91d516166be758b303f3
hnren@aibee.com <kawayi_rendroid@163.com>
2019-07-03 03:50:43 +0000

    add setup refineDet

--- refineDet/dw_pretrain_data.sh
+++ refineDet/dw_pretrain_data.sh
@@ -2,3 +2,6 @@ echo "download pretrained Vgg"
 wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
 echo "copy VOC2007"
 cp /data_public/VOC/voc2007/VOCtrainval_06-Nov-2007.tar .
+echo "restore last checkpoint"
+mkdir refinedet320
+cp /data/refinedet_trainout/* refinedet320/
