84a86f5b5f423d2a0b668facab0fa953a539ecf0
kawayi_rendroid@163.com <kawayi_rendroid@163.com>
2019-07-26 19:25:29 +0800

    1. add log for scripts/create_annoset.py 2. change test/unds_metrics's some shell snytax, which is still ill to work

--- refineDet_caffe/test/unds_metrics/1_create_metricstest_data.sh
+++ refineDet_caffe/test/unds_metrics/1_create_metricstest_data.sh
@@ -1,7 +1,10 @@
-cur_dir=$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
-root_dir=$cur_dir/../..
+#cur_dir=$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
+cur_dir="/ssd/hnren/tf/tf_base/refineDet_caffe/test/unds_metrics/"
+#root_dir=$cur_dir/../..
+root_dir="/ssd/hnren/tf/tf_base/refineDet_caffe/test/unds_metrics/"
 
-cd $root_dir
+#cd $root_dir
+cd /ssd/hnren/tf/tf_base/refineDet_caffe/test/unds_metrics
 
 redo=1
 data_root_dir="/ssd/hnren/tf/tf_base/refineDet_caffe/test/unds_metrics/VOCdevkit/"
@@ -14,11 +17,8 @@ max_dim=0
 width=0
 height=0
 
-extra_cmd="--encode-type=jpg --encoded"
-if [ $redo ]
-then
-  extra_cmd="$extra_cmd --redo"
-fi
+extra_cmd="--encode-type=jpg --encoded --redo"
+
 for subset in trainval test
 do
   echo " python $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir /ssd/hnren/tf/tf_base/refineDet_caffe/data/VOC0712/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name"
